b63b534c583c3d6c2886b8c102d0701622c15e71
93lucasp <spezzanoluca@gmail.com>
2019-11-10 14:32:32 +0100

    added articles events and snippets

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,26 +4,3 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
-const axios = require('axios')
-
-module.exports = function (api) {
-  api.loadSource(async actions => {
-    const { data } = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/notonlycss')
-
-    const collection = actions.addCollection('RedditPost')
-
-    for (const post of data.items) {
-      collection.addNode({
-        id: post.guid.replace('https://medium.com/p/',""),
-        title: post.title,
-        path: '/reddit/' + post.guid.replace('https://medium.com/p/',""),
-        thumbnail: post.thumbnail,
-        date: post.pubDate.split(" ")[0],
-        author: post.author,
-        content: post.content,
-        link: post.link
-      })
-    }
-  })
-}
