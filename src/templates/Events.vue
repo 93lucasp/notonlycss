c543fa08c7baeb654470dc522a1ad0418f5ac116
93lucasp <spezzanoluca@gmail.com>
2019-11-16 10:51:52 +0100

    added style for event

--- src/templates/Events.vue
+++ src/templates/Events.vue
@@ -1,8 +1,11 @@
 <template>
   <Layout>
     <div class="eventPage">
-      <h1>{{$page.event.title}}</h1>
-      <div class="eventPage__image" :style="{ 'background-image': 'url(' + $page.event.image + ')' }"></div>
+      <h1 class="text-3xl md:text-4xl font-bold mb-3">{{$page.event.title}}</h1>
+      <div
+        class="eventPage__image"
+        :style="{ 'background-image': 'url(' + $page.event.image + ')' }"
+      ></div>
       <!-- <i><time>{{$page.event.date}}</time></i> -->
       <div v-html="$page.event.content"></div>
     </div>
@@ -29,19 +32,47 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="scss">
 .eventPage {
   background-color: #0e2d43;
   padding: 30px;
   margin-bottom: 100px;
+  h1 {
+    font-size: 2em;
+    font-weight: bold;
+  }
+  h2 {
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+  h3 {
+    font-size: 1.17em;
+    font-weight: bold;
+  }
+  h4 {
+    font-size: 1em;
+    font-weight: bold;
+  }
+  h5 {
+    font-size: .83em;
+    font-weight: bold;
+  }
+  h6 {
+    font-size: .67em;
+    font-weight: bold;
+  }
+  p {
+    margin-bottom: 20px;
+    font-size: 18px;
+  }
 }
 .eventPage__image {
-   width: 100%;
-    height: 400px;
-    background-position: center;
-    background-size: cover;
-    border-radius: 5px;
-    margin-bottom: 30px;
+  width: 100%;
+  height: 400px;
+  background-position: center;
+  background-size: cover;
+  border-radius: 5px;
+  margin-bottom: 30px;
 }
 </style>
 
