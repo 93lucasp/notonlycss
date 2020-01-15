2053cc2a41b420d77551c2cfd05cef344974f094
Luca Spezzano <spezzanoluca@gmail.com>
2020-01-15 09:25:23 +0100

    changed copy

--- src/pages/Newsletter.vue
+++ src/pages/Newsletter.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout> 
     <h1 class="text-3xl md:text-4xl font-bold mb-3">Newsletter <i class="far fa-envelope ml-2 text-2xl"></i></h1>
-    <h2 class="text-xl md:text-2xl mb-4">The best <b>Frontend articles</b> in your inbox weekly</h2>
+    <h2 class="text-xl md:text-2xl mb-4">The best <b>Frontend articles</b> in your inbox monthly</h2>
     <p class="text-lg md:text-xl font-light">The Not Only CSS newsletter will tell you something new every week.</p>
-    <p class="text-lg md:text-xl mb-12 font-light">We publish articles <b>every Tuesday</b>, you can receive them <b>directly in your inbox, just join our newsletter!</b> </p>
+    <p class="text-lg md:text-xl mb-12 font-light">We publish articles <b>every month</b>, you can receive them <b>directly in your inbox, just join our newsletter!</b> </p>
     <a href="https://upscri.be/ekrivb" target="_blank" rel="noopener noreferrer" class="btn-cta py-2 px-10 rounded">Join now</a>
 
     
