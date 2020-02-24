2dc79992b5e2009cedf034544dc4cb8685f64b87
Luca Spezzano <spezzanoluca@gmail.com>
2020-02-24 17:30:08 +0100

    test form

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -37,7 +37,30 @@
 
 <script>
 export default {
-  metaInfo: {}
+  data() {
+  return {
+    formData: {},
+  }
+},
+methods: {
+  encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  },
+  handleSubmit(e) {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: this.encode({
+        'form-name': e.target.getAttribute('name'),
+        ...this.formData,
+      }),
+    })
+    .then(() => this.$router.push('/success'))
+    .catch(error => alert(error))
+  }
+}
 };
 </script>
 <style scoped>
