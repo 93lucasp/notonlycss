22e2eb21cd5e0566e5d201301a47458df9145676
Luca Spezzano <spezzanoluca@gmail.com>
2020-02-24 17:29:27 +0100

    test form

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,28 +1,37 @@
 <template>
   <Layout>
-    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-      <p class="hidden">
-        <label>
-          Don’t fill this out if you're human:
-          <input name="bot-field" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email:
-          <input type="text" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message:
-          <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit" class="btn">Send</button>
-      </p>
-    </form>
+   <form 
+  name="contact"
+  method="post"
+  v-on:submit.prevent="handleSubmit"
+  action="/success/"
+  data-netlify="true"
+  data-netlify-honeypot="bot-field"
+>
+  <input type="hidden" name="form-name" value="contact" />
+  <p hidden>
+    <label>
+      Don’t fill this out: <input name="bot-field" />
+    </label>
+  </p>
+  <div class="sender-info">
+    <div>
+      <label for="name" class="label" >Your name</label>
+      <input type="text" name="name" v-model="formData.name" />
+    </div>
+    <div>
+      <label for="email">Your email</label>
+      <input type="email" name="email" v-model="formData.email" />
+    </div>
+  </div>
+
+  <div class="message-wrapper">
+    <label for="message">Message</label>
+    <textarea name="message" v-model="formData.message"></textarea>
+  </div>
+
+  <button type="submit">Submit form</button>
+</form>
   </Layout>
 </template>
 
